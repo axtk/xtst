656f7399ed1b7beb89b0b0cf2b8a8df0287b8b69
axtk <axtkx@inbox.ru>
2026-02-24 00:24:32 +0300

    remove useURLState

--- src/react/useURLState.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { usePortableState } from "./usePortableState.ts";
-import { URLContext } from "./URLContext.ts";
-import { RenderCallback } from "../types/RenderCallback.ts";
-import { NavigationOptions } from "../types/NavigationOptions.ts";
-
-export function useURLState(callback?: RenderCallback<NavigationOptions>) {
-  return usePortableState(useContext(URLContext), callback);
-}
